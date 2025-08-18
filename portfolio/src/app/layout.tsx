1c0931159f32ab679cbe4558f6650111f6046e3f
aryanvalvi <aryanvalvi323@gmail.com>
2025-08-18 13:23:21 +0530

    done

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -12,8 +12,60 @@ const inter = Inter({
 })
 
 export const metadata: Metadata = {
-  title: "AryanValvi",
-  description: "bang portfolio by aryan",
+  title: "Aryan Valvi",
+  description:
+    "Portfolio of Aryan Valvi - Next.js Developer, MERN Stack Developer, and Full Stack Engineer.",
+  keywords: [
+    "Aryan Valvi",
+    "Full Stack Developer",
+    "MERN Stack",
+    "React.js",
+    "Next.js",
+    "Node.js",
+    "Portfolio",
+  ],
+  authors: [{name: "Aryan Valvi", url: "https://aryanvalvi.com"}],
+  creator: "Aryan Valvi",
+  icons: {
+    icon: "/mainicon.png",
+    apple: "/mainicon.png",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://aryanvalvi.com",
+    title: "Aryan Valvi | Full Stack Developer",
+    description:
+      "Portfolio of Aryan Valvi - MERN Stack Developer, React.js Specialist, and Full Stack Engineer.",
+    siteName: "Aryan Valvi",
+    images: [
+      {
+        url: "https://aryanvalvi.com/aryan.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Aryan Valvi Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Aryan Valvi | Full Stack Developer",
+    description:
+      "Portfolio of Aryan Valvi - MERN Stack Developer, React.js Specialist, and Full Stack Engineer.",
+    images: ["https://aryanvalvi.com/aryan.jpeg"],
+    creator: "@Aryan___valvi",
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 }
 
 export default function RootLayout({
@@ -23,6 +75,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/mainicon.png" />
+      </head>
       <body
         className={`${inter.className} antialiased bg-neutral-100 dark:bg-neutral-700`}
       >
